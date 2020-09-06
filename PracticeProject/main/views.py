8b33f9821382c2842150e0dfe8fa93b575767118
jana-choi <lovej0918@gmail.com>
2020-09-06 13:41:58 +0900

    project, app setting signup login, logout

---
+++ PracticeProject/main/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from signup.models import *
+import hashlib
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+def index(request):
+    if not "user_id" in request.session.keys():
+        return redirect("main_loginView")
+
+    return render(request, "main/index.html")
+
+def loginView(request):
+    if "user_id" in request.session.keys():
+        return redirect("main_index")
+
+    return render(request, "main/login.html")
+
+def login(request):
+    if "user_id" in request.session.keys():
+        return redirect("main_index")
+
+    user_input_id = request.POST["loginEmail"]
+    user_input_pw = request.POST["loginPW"]
+    try:
+        user = User.objects.get(user_id = user_input_id)
+        encoded_userPW = user_input_pw.encode()
+        encrypted_userPW = hashlib.sha256(encoded_userPW).hexdigest()
+
+        if encrypted_userPW == user.user_pw:
+            request.session["user_id"] = user.user_id
+            request.session["user_name"] = user.user_name
+            return redirect("main_index")
+        else:
+            return redirect("main_loginView")
+    
+    except ObjectDoesNotExist:
+        message = "이메일이 존재하지 않습니다."
+        return render(request, "main/error.html", {"message": message})
+
+    except:
+        message = "알 수 없는 오류가 발생했습니다."
+        return render(request, "main/error.html", {"message": message})
+    
+def logout(request):
+    if "user_id" in request.session.keys():
+        del request.session["user_id"]
+    if "user_name" in request.session.keys():
+        del request.session["user_name"]
+    if "code" in request.session.keys():
+        del request.session["code"]
+    return redirect("main_loginView")
